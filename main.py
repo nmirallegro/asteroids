1e4584cee23aa14c0b24b752e3f12fe2d273edae
nmirallegro <mirallegron@gmail.com>
2024-10-27 17:44:16 -0400

    new features

--- main.py
+++ main.py
@@ -2,13 +2,19 @@
 # the open-source pygame library
 # throughout this file
 import pygame
-from constants import *
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 
 def main():
-    print("Starting asteroids!")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    while True:
+        screen.fill((0,0,0))
+        pygame.display.flip()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
 
 if __name__ == "__main__":
     main()
