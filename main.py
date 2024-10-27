bf4f886d46150623c981ab14ba5832125cded239
nmirallegro <mirallegron@gmail.com>
2024-10-27 19:45:10 -0400

    new features

--- main.py
+++ main.py
@@ -7,10 +7,14 @@ from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+    clock = pygame.time.Clock()
+    dt = 0
+    
     while True:
         screen.fill((0,0,0))
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
