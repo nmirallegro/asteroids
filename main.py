a4d8f92e5b7505b3fd830e32a28f14e17dfe0a33
nmirallegro <mirallegron@gmail.com>
2024-10-29 20:06:17 -0400

    new features

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
@@ -10,20 +12,37 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+
     player = Player((SCREEN_WIDTH/2),(SCREEN_HEIGHT/2))
+    
 
     while True:
-        screen.fill((0,0,0))
-        player.draw(screen)
-        player.update(dt)
-        pygame.display.flip()
-        clock.tick(60)
-        dt = clock.tick(60) / 1000
-        
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        screen.fill((0,0,0))
+
+        for obj in updatable:
+            obj.update(dt)
+
+        for obj in drawable:
+            obj.draw(screen)
+
+
+        #player.draw(screen)
+        #player.update(dt)
+        pygame.display.flip()
+        #clock.tick(60)
+        dt = clock.tick(60) / 1000
+        
+
+
 
 if __name__ == "__main__":
     main()
